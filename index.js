c9a37989d74c5952bdc98b86fc2a6354d15f5dc0
NIck Marucci <npm1514@gmail.com>
2021-04-26 14:58:55 -0600

    remove script

--- index.js
+++ index.js
@@ -44,11 +44,6 @@ app.get('/', (req, res) => {
           <script src="js/owl.carousel.js"></script>
           <script src="js/owl.carousel.min.js"></script>
           <script src="js/jquery.matchHeight.js"></script>
-          <script>
-            if(window.location.href != "https://www.fandmccoaching.com/"){
-              window.location.href == "https://www.fandmccoaching.com/";
-            }
-          </script>
       </head>
       <body>
 
